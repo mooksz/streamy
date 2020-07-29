fc1a7938a56bc0d511465ee6ed69a780c7211be0
Mike Haklander <mike@the-pack.nl>
2020-07-29 10:53:50 +0200

    first commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+//Always visible components
+import Header from "./Header";
+
+//Streams
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+
+const App = () => {
+	return (
+		<div className="ui container">
+			<BrowserRouter>
+				<Header />
+
+				<Route path="/" exact component={StreamList} />
+				<Route path="/streams/new" component={StreamCreate} />
+				<Route path="/streams/delete" component={StreamDelete} />
+				<Route path="/streams/edit" component={StreamEdit} />
+				<Route path="/streams/show" component={StreamShow} />
+			</BrowserRouter>
+		</div>
+	);
+};
+
+export default App;
